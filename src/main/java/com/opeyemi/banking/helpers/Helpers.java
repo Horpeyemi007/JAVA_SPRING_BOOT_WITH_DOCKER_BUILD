081dc1632636dea3a4da6bb1cc46a993119ac77d
Opeyemi <Opeyemidevops@gmail.com>
2024-11-23 20:36:24 +0100

    added source code for spring boot app

---
+++ src/main/java/com/opeyemi/banking/helpers/Helpers.java
@@ -0,0 +1,27 @@
+package com.opeyemi.banking.helpers;
+
+import java.math.BigDecimal;
+
+public class Helpers {
+  public static String generateAcccountNumber() {
+    int n = 10;
+    String numbers = "01234567890";
+    StringBuilder sb = new StringBuilder(n);
+    for(int i = 0; i < n; i++){
+      int randonNumber = (int)(numbers.length() * Math.random());
+      sb.append(numbers.charAt(randonNumber));
+    }
+    return sb.toString();
+  }
+
+  public static BigDecimal creditExistingBalance(BigDecimal currentBalance, String amountToCredit){
+    BigDecimal creditAmout = new BigDecimal(amountToCredit);
+    return currentBalance.add(creditAmout);
+  }
+
+  public static BigDecimal debitExistingBalance(BigDecimal currentBalance, String amountToDebit){
+    BigDecimal debitAmout = new BigDecimal(amountToDebit);
+    return currentBalance.add(debitAmout);
+  }
+
+}
